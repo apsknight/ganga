93469bfee629b2d381bbe71d42a1d4a739f30a3d
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-07-09 18:49:34 +0530

    Code Cleaning

--- ganga/GangaCore/Core/GangaRepository/GangaRepositoryXML.py
+++ ganga/GangaCore/Core/GangaRepository/GangaRepositoryXML.py
@@ -535,9 +535,6 @@ class GangaRepositoryLocal(GangaRepository):
             firstRun (bool): If this is the call from the Repo startup then load the master index for perfomance boost
         """
 
-        # if len(self.newly_added) > 0:
-        #     self.load(self.newly_added)
-
         # First locate and load the index files
         logger.debug("updating index...")
         objs = self.get_index_listing()
@@ -557,6 +554,7 @@ class GangaRepositoryLocal(GangaRepository):
             if this_id > self.sessionlock.count:
                 self.sessionlock.count = this_id + 1
             # Locked IDs can be ignored
+            # Commenting this because for Job Sharing, we shouldn't ignore locked IDs.
             # if this_id in locked_ids:
             #     continue
             # Skip corrupt IDs
@@ -644,8 +642,6 @@ class GangaRepositoryLocal(GangaRepository):
             isShutdown = not firstRun
             self._write_master_cache(isShutdown)
 
-        # self.load(changed_ids)
-        # self.newly_added = self.newly_added + changed_ids
         return changed_ids
 
     def add(self, objs, force_ids=None):
@@ -862,10 +858,6 @@ class GangaRepositoryLocal(GangaRepository):
         """
 
         # If this_id is not in the objects add the object we got from reading the XML
-        # print '>fn', fn
-        # print '>this_id', this_id
-        # print '>load_backup', load_backup
-        # print '>has_children', has_children
 
         need_to_copy = True
         if this_id not in self.objects:
@@ -873,8 +865,7 @@ class GangaRepositoryLocal(GangaRepository):
             need_to_copy = False
 
         obj = self.objects[this_id]
-        # print '>obj', obj
-        # print '>need_to_copy', need_to_copy
+
         # If the object was already in the objects (i.e. cache object, replace the schema content wilst avoiding R/O checks and such
         # The end goal is to keep the object at this_id the same object in memory but to make it closer to tmpobj.
         # TODO investigate changing this to copyFrom
@@ -885,8 +876,6 @@ class GangaRepositoryLocal(GangaRepository):
             for attr_name, attr_val in obj._schema.allItems():
                 if attr_name not in tmpobj._data:
                     obj.setSchemaAttribute(attr_name, obj._schema.getDefaultValue(attr_name))
-
-        # print '>has_children', has_children
 
         if has_children:
             logger.debug("Adding children")
@@ -900,22 +889,21 @@ class GangaRepositoryLocal(GangaRepository):
                     from GangaCore.GPIDev.Lib.GangaList.GangaList import GangaList
                     def_val = GangaList()
                 obj.setSchemaAttribute(self.sub_split, def_val)
-        # print 'check1'
+
         from GangaCore.GPIDev.Base.Objects import do_not_copy
         for node_key, node_val in obj._data.items():
             if isType(node_val, Node):
                 if node_key not in do_not_copy:
                     node_val._setParent(obj)
-        # print 'check2'
+
         # Check if index cache; if loaded; was valid:
         if obj._index_cache not in [{}]:
             self._check_index_cache(obj, this_id)
-        # print 'check3'
+
         obj._index_cache = {}
 
         if this_id not in self._fully_loaded:
             self._fully_loaded[this_id] = obj
-        # print 'check4'
 
     def _load_xml_from_obj(self, fobj, fn, this_id, load_backup):
         """
@@ -927,8 +915,6 @@ class GangaRepositoryLocal(GangaRepository):
             this_id (int): This is the key of the object in the objects dict where the output will be stored
             load_backup (bool): This reflects whether we are loading the backup 'data~' or normal 'data' XML file
         """
-        # print '>', fobj, fn, this_id, load_backup
-        # print '>', load_backup
         b4=time.time()
         tmpobj, errs = self.from_file(fobj)
         a4=time.time()
@@ -949,8 +935,8 @@ class GangaRepositoryLocal(GangaRepository):
         logger.debug("Found children: %s" % str(has_children))
         try:
             self._parse_xml(fn, this_id, load_backup, has_children, tmpobj)
-        except Exception as e:
-            print '$$', e
+        except Exception as err:
+            logger.error("Error: %s" % err)
 
 
         if hasattr(self.objects[this_id], self.sub_split):
